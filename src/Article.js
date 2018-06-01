2c41f176f3644c25420fbb6fbd51dfaad6f57563
Nicolas Crovatti <nicolas.crovatti@gmail.com>
2018-06-01 13:02:25 +0200

    Updating demo with all cleanup cases

---
+++ src/Article.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import TeadsAd from './TeadsAd';
+
+const Article = () => (
+  <div>
+    <h1>Article Title</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam at neque elementum velit dictum rhoncus in in felis. Sed egestas tempus diam ac interdum. Nulla lobortis massa suscipit semper ullamcorper. Cras dignissim posuere lacus id volutpat. Donec cursus aliquet laoreet. Pellentesque fermentum sagittis sem. In hac habitasse platea dictumst. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;</p>
+    <TeadsAd  pId={84885} />
+    <p>Quisque feugiat maximus nulla, dictum dapibus ante posuere lacinia. Nunc tempus, nisi ut convallis viverra, justo arcu cursus metus, a tristique augue orci vitae felis. Duis egestas mi id lobortis pharetra. Vestibulum non orci turpis. Nulla non velit nec magna sodales facilisis ut eu enim. Sed eget nisl at felis cursus dignissim. Aliquam eleifend maximus odio, in ultrices urna ullamcorper quis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec lacinia justo vitae felis blandit scelerisque. In varius sem eu ante lobortis semper. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+    <p>Sed ut dui lectus. Donec dictum et purus sit amet malesuada. Maecenas interdum, libero non tempus condimentum, nulla enim ultricies lorem, quis lacinia magna risus vitae justo. Aliquam ac dolor odio. In vel risus non ligula porta ultrices et nec urna. Maecenas pharetra dolor ex, ut consectetur urna iaculis dignissim. Sed magna justo, lacinia ut nulla sit amet, finibus iaculis lorem. Duis id neque pretium, posuere purus sit amet, mattis est. Nullam sagittis ultrices velit. Phasellus venenatis sagittis mi, in finibus est aliquet id. Aliquam accumsan felis eget est vehicula, egestas malesuada leo semper.</p>
+  </div>
+)
+
+export default Article; 
---
+++ src/TeadsAd.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class TeadsAd extends Component {
+
+  componentDidMount() {
+    const { pId } = this.props;
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.id = 'teadstag'
+    script.src = `//a.teads.tv/page/${pId}/tag`;
+    script.async = true;
+    document.body.appendChild(script);
+  }
+
+  componentWillUnmount() {
+    try {
+      document.body.removeChild(document.getElementById("teadstag"));
+      document.body.removeChild(document.getElementById("teadsusersync"));
+      window.teads.pageInstance.clearInstances();
+      delete window.teads.page;
+      delete window.teadsscript;
+      document.querySelectorAll(".teads-inline-style").forEach(function(e) {e.remove()});
+      document.querySelectorAll("script[src='//cdn.teads.tv/media/format/v3/teads-format.min.js']").forEach(function(e){e.remove()})
+    } catch (e) {}
+  }
+
+  render() {
+    const { className } = this.props;
+    return <div id="teads-experience" className={className} />;
+  }
+}
+
+TeadsAd.propTypes = {
+  className: PropTypes.string,
+  pId: PropTypes.number.isRequired,
+};
+
+TeadsAd.defaultProps = {
+  className: null,
+};
+
+export default TeadsAd;
